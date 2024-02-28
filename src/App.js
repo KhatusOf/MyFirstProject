c1db26791f2a9e76df097cf49e6102a12b68e743
KhatusOf <vas.andr04@mail.ru>
2024-02-28 20:50:24 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App() {
-  return (
-    <div className="App">
-    
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default function App(){
+  return(
+    <div className="wrapper">
+      <Header/>
+      <Footer/>
     </div>
   );
 }
-
-export default App;
